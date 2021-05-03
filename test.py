3933709f92eb8d74fbe3d4e4354c9b15b229f7c3
jackyman1997 <jackyman1997@gmail.com>
2021-05-03 20:55:01 +0100

    upload new stuff

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-from my_tools.maths.factorials import nCr
+# asdfuhasdkjfhfasd
+import os
+import pathlib
+# %%
+[item if os.path.isfile(item) else os.listdir(item) for item in os.listdir()]
+# %%
+words = ['apple', 'banana', 'orange']
 
-print(nCr(10, 5))
+words_to_len = {word if word != 'apple' else 'not apple': len(word) if word != 'apple' else 'not apple'  for word in words}
+print(words_to_len)
+# %%
+import my_tools.maths
+
+my_tools.maths.factorials.factorial(10)
+# %%
